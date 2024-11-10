6559288f547237a273113cb3de12c19a97ab0f95
MarkMutt <lleasy8@gmail.com>
2024-11-10 15:41:47 -0500

    cleaned up index file

--- test-apps/remix-vite-cjs/app/routes/_index.tsx
+++ test-apps/remix-vite-cjs/app/routes/_index.tsx
@@ -1,5 +1,4 @@
 import type { MetaFunction } from "@remix-run/node";
-import { test } from "food-pantry-db";
 
 export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
