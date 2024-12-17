ef0ef362dd61f29dea238c1b005c9b41418d015b
MarkMutt <lleasy8@gmail.com>
2024-12-17 18:50:16 -0500

    adding docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,2 @@
+window.navigationData =
+	"eJyFzbsKwkAQheF3mXoxGrzg1ppa0E4sVjOawWR22T2KUfLuYhUCQuqf853jh8AvkKXtkxV7uBuToeBQkSW0gVPWl0mFpiZDd9GSbD5dr2aLvDND49CG/8QvjAmiAnG1vLmQyAk+cuF9uXOK2G7OvXt96AXiNWUjk+Hjct6dvo0+VFw="
